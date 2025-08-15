0922c08393144f9f577e3bbe96e459bf801256de
Rudy V <eagleeye1689@gmail.com>
2025-08-14 19:26:51 -0500

    Solution Coded

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -8,8 +8,10 @@ public class StudentManager {
     private Scanner scanner = new Scanner(System.in);
     private List<String> students = new ArrayList<>();
 
+    String newLine = System.lineSeparator();
+
     public void mainMenu() {
-        System.out.println("Welcome to the student manager!");
+        System.out.println(newLine + "Welcome to the student manager!");
         System.out.println("1. Add student");
         System.out.println("2. Remove student");
         System.out.println("3. View all students");
@@ -27,7 +29,7 @@ public class StudentManager {
             System.out.println("Goodbye!");
             System.exit(0);
         } else {
-            System.out.println("Invalid choice. Please try again.");
+            System.out.println(newLine + "Invalid choice. Please try again.");
         }
     }
 
@@ -35,7 +37,9 @@ public class StudentManager {
         // Prompt the user to enter a new student name (using scanner and I/O methods learned previously,
         // refer to mainMenu() for an example)
         // Add the student to the list
-
+        System.out.println(newLine + "Please enter the student's name: ");
+        String name = scanner.nextLine();
+        students.add(name);
         mainMenu();
     }
 
@@ -43,13 +47,22 @@ public class StudentManager {
         // Prompt the user for a student name
         // Use the contains method to check if the student entered is in the list
         // If so, remove it, if not, print "Student not found."
-
+        System.out.println(newLine + "Please enter the name of the student to remove: ");
+        String name = scanner.nextLine();
+        if(students.contains(name)){
+            students.remove(name);
+            System.out.println(name + "has been removed");
+        }
         mainMenu();
     }
 
     public void viewStudents() {
         // Loop through the list of students and print each one
         // (Use a for-each loop!)
+        System.out.println(newLine + "STUDENTS");
+        for (String student : students){
+            System.out.println(student);
+        }
         mainMenu();
     }
 }
